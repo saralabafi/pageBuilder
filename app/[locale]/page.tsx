b021bacb9dde6924b156e9696387bcbfaebf8767
AmirHossein <amir73khakbaz@gmail.com>
2023-07-01 16:15:22 +0330

    remove unless lines

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,11 +1,7 @@
 'use client'
-import { Select } from 'components/Select/Select'
-import { TextArea } from 'components/TextArea/TextArea'
 import Link from 'next/link'
-import { useState } from 'react'
 
 export default function Home() {
-  const [first, setfirst] = useState('')
   return (
     <div className="flex-col -full">
       <div className="flex justify-center items-center">
@@ -13,7 +9,6 @@ export default function Home() {
         <Link className="mx-4 cursor-pointer" href="/testView">
           For view test Page
         </Link>
-        <TextArea onChange={(e) => setfirst(e)} value={first} />
       </div>
     </div>
   )
