8820f1977ef20e6933ebdfbe26664603cf2ac825
javad <javadMohammadi.js@gmail.com>
2023-07-03 13:07:04 +0330

    add interface for  comboBox props

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,7 +2,7 @@
 import ComboBox from 'components/ComboBox/ComboBox'
 import Switch from 'components/Switch/Switch'
 import Link from 'next/link'
-
+import { options } from 'components/ComboBox/ComboBox.constants'
 export default function Home() {
   return (
     <div className="flex-col w-full h-screen bg-white text-black">
@@ -14,7 +14,7 @@ export default function Home() {
       <div className='text-center w-full flex justify-center items-center flex-col mt-5'>
         <div className='mt-3'>
           <span>ComboBox</span>
-          <ComboBox />
+          <ComboBox defaultValue='Select an option' id='select color' options={options} size='small' />
         </div>
         <div className='mt-3'>
           <span>Switch</span>
--- components/ComboBox/ComboBox.types.ts
+++ components/ComboBox/ComboBox.types.ts
@@ -1,4 +1,19 @@
+import { Properties } from 'csstype'
 export interface Option {
   content: string
   id: string
+}
+export interface ComboBoxProp {
+  options: Option[]
+  disabled?: boolean
+  defaultValue: string
+  size: 'small' | 'medium'
+  readonly?: boolean
+  open?: boolean
+  openOnFocus?: boolean
+  notfindtext?: string
+  inputValue?: string
+  id: string
+  sx?: Properties<string | number, string & {}>
+  activeBorder?: string
 }
