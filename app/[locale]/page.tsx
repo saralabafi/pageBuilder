e5eb66193d7652219eb9a7784d39d3dafa613a4d
javad <javadMohammadi.js@gmail.com>
2023-07-03 12:22:30 +0330

    change file name and add comboBox to root page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,13 +1,25 @@
 'use client'
+import ComboBox from 'components/ComboBox/ComboBox'
+import Switch from 'components/Switch/Switch'
 import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="flex-col -full">
+    <div className="flex-col w-full h-screen bg-white text-black">
       <div className="flex justify-center items-center">
         <Link className="mx-4 cursor-pointer" href="/testView">
           For view test Page
         </Link>
+      </div>
+      <div className='text-center w-full flex justify-center items-center flex-col mt-5'>
+        <div className='mt-3'>
+          <span>ComboBox</span>
+          <ComboBox />
+        </div>
+        <div className='mt-3'>
+          <span>Switch</span>
+          {/* <Switch  /> */}
+        </div>
       </div>
     </div>
   )
