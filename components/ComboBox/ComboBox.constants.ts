84c44446327255b0f6190975c6094cabdf221af5
javad <javadMohammadi.js@gmail.com>
2023-07-03 12:23:45 +0330

    create types and constants file keep data

---
+++ components/ComboBox/ComboBox.constants.ts
@@ -0,0 +1,9 @@
+import { Option } from './ComboBox.types'
+export const options: Option[] = [
+  { content: 'red', id: 'Red' },
+  { content: 'green', id: 'Green' },
+  { content: 'blue', id: 'Blue' },
+  { content: 'Violet', id: 'Violet' },
+  { content: 'Purple', id: 'Purple' },
+  { content: 'Fuchsia', id: 'Fuchsia' },
+]
