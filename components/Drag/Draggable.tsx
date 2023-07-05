4da701bafd04b2ce0024eb2047b1d6fe9351c095
AmirHossein <amir73khakbaz@gmail.com>
2023-07-05 10:16:43 +0300

    implement structure design page

---
+++ components/Drag/Draggable.tsx
@@ -0,0 +1,21 @@
+'use client'
+import { useDraggable } from '@dnd-kit/core'
+
+export function Draggable(props: IDraggable) {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: props.id,
+    data: { title: props.children },
+  })
+
+  const style = transform
+    ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+      }
+    : undefined
+
+  return (
+    <button ref={setNodeRef} style={style} {...listeners} {...attributes}>
+      {props.children}
+    </button>
+  )
+}
---
+++ components/Drag/Droppable.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+import { useDroppable } from '@dnd-kit/core'
+
+export function Droppable(props: IDroppable) {
+  const { setNodeRef } = useDroppable({
+    id: 'cart-droppable',
+  })
+
+  return (
+    <div className="w-full h-full border-2 border-gray-500 " ref={setNodeRef}>
+      {props.children}
+    </div>
+  )
+}
