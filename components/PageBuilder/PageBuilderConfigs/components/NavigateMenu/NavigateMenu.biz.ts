76b9fb92bf9a5628a234bcbda5bb13ea2a2dceef
AmirHossein <amir73khakbaz@gmail.com>
2023-08-01 10:12:32 +0430

    create Tree component and use it instead sortableTree

--- components/PageBuilder/PageBuilderConfigs/components/NavigateMenu/NavigateMenu.biz.ts
+++ components/PageBuilder/PageBuilderConfigs/components/NavigateMenu/NavigateMenu.biz.ts
@@ -5,25 +5,13 @@ import { RootState } from 'redux/Store'
 
 export const useNavigateMenu = () => {
   const { designList } = useSelector((state: RootState) => state.pageDesign)
-  const t = useTranslations('layout')
-  const dispatch=useDispatch()
   const sortableItems = JSON.stringify(designList)
-  const calculatePadding = (clone: any, depth: any, indentationWidth: any) => {
-    const array: any = {
-      0: 'ps-0',
-      10: 'ps-[10px]',
-      20: 'ps-[20px]',
-      30: 'ps-[30px]',
-      40: 'ps-[50px]',
-      50: 'ps-[60px]',
-    }
-
-    return array[clone ? indentationWidth : indentationWidth * depth]
-  }
+  const t = useTranslations('layout')
+  const dispatch = useDispatch()
 
   const handleChange = (e: any) => {
     dispatch(setDesignList(e))
   }
 
-  return { sortableItems, t, calculatePadding, handleChange }
+  return { sortableItems, t, handleChange }
 }
--- components/PageBuilder/PageBuilderConfigs/components/NavigateMenu/NavigateMenu.tsx
+++ components/PageBuilder/PageBuilderConfigs/components/NavigateMenu/NavigateMenu.tsx
@@ -1,44 +1,14 @@
-import { Flex } from 'components/CoreComponents/Flex/Flex'
-import Text from 'components/CoreComponents/Text/Text'
-import {
-  SimpleTreeItemWrapper,
-  SortableTree,
-  TreeItemComponentProps,
-} from 'dnd-kit-sortable-tree'
-import React from 'react'
+import { TreeView } from 'components/TreeView/TreeView'
 import { useNavigateMenu } from './NavigateMenu.biz'
 
 export const NavigateMenu = () => {
-  const { t, calculatePadding, handleChange, sortableItems } = useNavigateMenu()
+  const { t, handleChange, sortableItems } = useNavigateMenu()
 
   return (
-    <SortableTree
-      items={JSON.parse(sortableItems)}
-      onItemsChanged={handleChange}
-      TreeItemComponent={React.forwardRef<
-        HTMLDivElement,
-        TreeItemComponentProps<{ id: string; type: string }>
-      >((props, ref) => {
-        return (
-          <SimpleTreeItemWrapper
-            {...props}
-            ref={ref}
-            showDragHandle={false}
-            className={calculatePadding(
-              props.clone,
-              props.indentationWidth,
-              props.depth
-            )}
-            contentClassName={`!border-none `}>
-            <Flex align="items-center" gap="gap-1" padding="p-1">
-              <div className="w-3 h-3 border border-dashed border-neutral-500 " />
-              <Text fontWeight={300} fontSize={12} color="text-neutral-500">
-                {t(props.item.type)}
-              </Text>
-            </Flex>
-          </SimpleTreeItemWrapper>
-        )
-      })}
+    <TreeView
+      sortableItems={sortableItems}
+      handleChange={handleChange}
+      renderText={(e: string) => t(e)}
     />
   )
 }
