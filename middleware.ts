479ea7f05780ef37e8871e62df68557cece65ea7
AmirHossein <amir73khakbaz@gmail.com>
2023-08-23 14:41:47 +0430

    edit localize message and layout

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,10 @@ import createMiddleware from 'next-intl/middleware'
 
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ['en', 'fa'],
+  locales: ['en-us', 'fa-ir'],
 
   // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
-  defaultLocale: 'fa',
+  defaultLocale: 'fa-ir',
 })
 
 export const config = {
