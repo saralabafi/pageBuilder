f067adeab712a8dddccf52d1104249f8d5dfbe38
Hesam Habibi <h.habibi@DESKTOP-3KTO69T>
2023-08-26 12:27:04 +0330

    cleane page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -5,11 +5,6 @@ import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import SwitchControl from 'components/Tools/ContentStructureBuilder/SwitchControl/SwitchControl'
 export default function Home() {
-  const handleToggleChange = (checked: boolean) => {
-    // console.log('Toggle changed:', checked)
-    // Handle the toggle change logic
-    // console.log('Toggle changed:', newToggle)
-  }
   return (
     <div className="flex-col w-full h-screen bg-white text-black">
       <div className="flex justify-center items-center">
@@ -17,17 +12,7 @@ export default function Home() {
           For view test Page
         </Link>
       </div>
-      <div className="flex-col w-full">
-        <SwitchControl
-          label="موافقت با قوانین و مقررات"
-          isChecked={false}
-          onChange={handleToggleChange}
-          disabled={false}
-          isMandatory={true}
-          errorMessage="لطفا این قسمت را تکمیل کنید"
-          helpText="با کلید روی این دکمه با قوانین سایت موافقت کنید"
-        />
-      </div>
+
       <div>
         <DndProvider backend={HTML5Backend}>
           <DndDesigner />
