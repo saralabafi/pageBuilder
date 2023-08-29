9838c1152acdb07e0f226dcf3b88b122e7c1d667
Hesam Habibi <h.habibi@DESKTOP-3KTO69T>
2023-08-29 14:33:11 +0330

    edit page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,18 +1,11 @@
 'use client'
 import DndDesigner from 'components/DndDesigner/DndDesigner'
-import DynamicOptions from 'components/Tools/ContentStructureBuilder/SingleSelection/DynamicOptions'
-import SingleSelection from 'components/Tools/ContentStructureBuilder/SingleSelection/SingleSelection'
+
 import Link from 'next/link'
-import { useState } from 'react'
+
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 export default function Home() {
-  const [selectedOptions, setSelectedOptions] = useState<string[]>([])
-
-  const handleOptionsChange = (options: string[]) => {
-    setSelectedOptions(options)
-  }
-
   return (
     <div className="flex-col w-full h-screen bg-white text-black">
       <div className="flex justify-center items-center">
@@ -20,14 +13,9 @@ export default function Home() {
           For view test Page
         </Link>
       </div>
-
-      <div>
-        <DynamicOptions title="عنوان" onOptionsChange={handleOptionsChange} />
-        <SingleSelection title="Test" options={selectedOptions} />
-      </div>
       <div>
         <DndProvider backend={HTML5Backend}>
-          {/* <DndDesigner /> */}
+          <DndDesigner />
         </DndProvider>
       </div>
     </div>
