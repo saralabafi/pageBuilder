7a4b14bc75caf2255e3eaa62112969833991bc3e
s.labafi <s.labafi@douran.com>
2023-08-30 07:02:39 +0330

    Refactoring TextSetting Style

--- components/Tools/ContentStructureBuilder/SettingType/TextSetting/TextSetting.tsx
+++ components/Tools/ContentStructureBuilder/SettingType/TextSetting/TextSetting.tsx
@@ -7,14 +7,14 @@ export const TextSetting = (props: ITextSettingProps) => {
   const locale = useLocale()
   return (
     <div className="w-full relative mb-3">
-      <label className="inline-block uppercase tracking-wide text-slate-700 text-[12px] font-bold mb-2">
+      <label className="inline-block uppercase tracking-wide text-slate-700 text-[12px] mb-2">
         {Title[locale]}
       </label>
       <input
         defaultValue={DefaultValue[locale]}
         readOnly={false}
         type="text"
-        className={`appearance-none block w-full bg-white text-gray-700 border text-[12px] rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white`}
+        className={`bg-gray-50 border border-slate-200 appearance-none block w-full h-[32px] bg-white text-gray-700 border text-[12px] rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white`}
         placeholder={PlaceHolder[locale]}
       />
     </div>
