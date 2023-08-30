93ce2bfb79a764eca941be02376e623273815f8b
Hesam Habibi <h.habibi@DESKTOP-3KTO69T>
2023-08-30 16:10:57 +0330

    clean page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,4 @@
 'use client'
-import { Flex } from 'components/CoreComponents/Flex/Flex'
-import { Select } from 'components/CoreComponents/Select/Select'
-import { DropDownSetting } from 'components/Tools/ContentStructureBuilder/SettingType/DropDownSetting/DropDownSetting'
 import Link from 'next/link'
 export default function Home() {
   return (
@@ -13,12 +10,6 @@ export default function Home() {
           locale="en-us">
           English
         </Link>
-        <hr></hr>
-        <div className="w-full">
-          <Select options={undefined} value={''} />
-          <DropDownSetting Source={{}} />
-        </div>
-
         <Link
           className="mx-4 cursor-pointer"
           href="/fa-ir/testView"
