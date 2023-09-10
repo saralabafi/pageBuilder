787ebca93d60c5bc01b9f9ad2467e533147d4d9e
s.labafi <s.labafi@douran.com>
2023-09-10 16:14:09 +0330

    Calling services

--- components/DndDesigner/DndDesigner.biz.ts
+++ components/DndDesigner/DndDesigner.biz.ts
@@ -1,7 +1,12 @@
-import { useCallback } from 'react'
+'use client'
+import { useCallback, useEffect } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { services } from 'services/services'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from 'redux/Store'
 import { Control, DropItem, DropZoneData } from './DndDesigner.type'
+import TT from '../../public/Data/CunstructingVisualBuilder.json'
+import { setDesignList } from 'redux/Design/Design'
 import shortid from 'shortid'
 import {
   selectActiveControl,
@@ -14,7 +19,27 @@ export const useDndDesigner = (renderList: any) => {
     (state: RootState) => state.pageDesign
   )
 
+  const { data } = useQuery(
+    [
+      {
+        url: 'cms/v1.0/{site}/pages/5E155016-EDC0-4364-D0D9-08DBB1CFC585',
+      },
+    ],
+    services.GetData
+  )
+
+  console.log('data from backis: ', data)
+
   const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(setDesignList(TT))
+  }, [])
+  // const { layout } = data
+
+  // const sara = data.layout
+  if (data) addParentId(data)
+
+  // addParentId(data)
 
   const handleDrop = useCallback(
     (dropZone: DropZoneData, item: DropItem) => {
@@ -50,4 +75,21 @@ export const useDndDesigner = (renderList: any) => {
     designList,
     handleDrop,
   }
+}
+
+const addParentId = (obj: any) => {
+  const newArray = []
+  console.log('adding parentId to : ', obj)
+  const layout = obj?.layout
+  const createParentId = (items: Control[], pId: any) => {
+    items.forEach((item: Control, i) => {
+      item.parentId = pId
+      console.log('do something')
+      if (item.Children) {
+        const pId = item.Id
+        createParentId(item.Children, pId)
+      }
+    })
+  }
+  createParentId(layout, 0)
 }
