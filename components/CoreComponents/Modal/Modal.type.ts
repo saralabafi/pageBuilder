23ef2322939f884ba223ca0200c985c9f927ba85
AmirHossein <amir73khakbaz@gmail.com>
2023-09-12 14:17:44 +0430

    fix api for render modal in create new content

--- components/CoreComponents/Modal/Modal.type.ts
+++ components/CoreComponents/Modal/Modal.type.ts
@@ -2,8 +2,9 @@ import { widthType } from 'types/width.type'
 
 export interface IModalProps {
   visible: boolean
-  title?: string 
+  title?: string
   onClose: () => void
   children: React.ReactNode
+  footer?: React.ReactNode
   width?: widthType
 }
