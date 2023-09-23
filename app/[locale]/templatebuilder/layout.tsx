61d58bba05cfe9eb2476fb30e03114df4c536ecb
Hesam Habibi <h.habibi@DESKTOP-3KTO69T>
2023-09-23 15:10:30 +0330

    edit after review

--- app/[locale]/templatebuilder/layout.tsx
+++ app/[locale]/templatebuilder/layout.tsx
@@ -1,4 +1,3 @@
-// TemplatebuilderLayout.js
 'use client'
 import { TemplateBuilderHeader } from 'components/TemplateBuilder/TemplateBuilderHeader/TemplateBuilderHeader'
 import { TemplateBuilderSideMenu } from 'components/TemplateBuilder/TemplateBuilderSideMenu/TemplateBuilderSideMenu'
@@ -9,15 +8,14 @@ import TemplateEditorCss from './templateEditorCss/page'
 import TemplateEditorJs from './templateEditorJs/page'
 import { useLayout } from '../formbuilder/Layout.biz'
 
-function TemplatebuilderLayout({ children }: { children: any }) {
+function TemplatebuilderLayout({ children }: { children: JSX.Element }) {
   const { activeTab } = useLayout()
-  const [activeComponent, setActiveComponent] = useState('HTML') // Initialize activeComponent state with 'HTML'
+  const [activeComponent, setActiveComponent] = useState('HTML')
 
   const handleComponentClick = (componentName: string) => {
     setActiveComponent(componentName)
   }
 
-  // Render the component based on the activeComponent state
   let component
   switch (activeComponent) {
     case 'HTML':
